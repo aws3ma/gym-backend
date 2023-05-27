57203362463a7d5aae7f3e75cae99c61624175e0
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-05-27 18:24:56 +0100

    edit date bug in paiement model

--- paiement/models.py
+++ paiement/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from authentication.models import User
 from bodybuilder.models import BodyBuilder
+from datetime import date
 # Create your models here.
 
 
 class Paiement(models.Model):
     amount = models.DecimalField(max_digits=6,decimal_places=1,null=False)
     bodybuilder = models.ForeignKey(BodyBuilder, on_delete=models.CASCADE)
-    start_date = models.DateField(auto_now_add=True, null=False)
-    due_date = models.DateField(auto_now_add=True, null=False)
+    start_date = models.DateField( null=False,default=date.today)
+    due_date = models.DateField( null=False,default=date.today)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     # paiement_date = models.DateTimeField(auto_now_add=True, null=False)
 
