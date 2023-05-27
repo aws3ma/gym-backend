5237acd724ee8f654248257dbcbb2482c836cb22
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-05-27 18:13:46 +0100

    add new attributes to paiement

--- paiement/models.py
+++ paiement/models.py
@@ -7,8 +7,10 @@ from bodybuilder.models import BodyBuilder
 class Paiement(models.Model):
     amount = models.DecimalField(max_digits=6,decimal_places=1,null=False)
     bodybuilder = models.ForeignKey(BodyBuilder, on_delete=models.CASCADE)
+    start_date = models.DateField(auto_now_add=True, null=False)
+    due_date = models.DateField(auto_now_add=True, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    paiement_date = models.DateTimeField(auto_now_add=True, null=False)
+    # paiement_date = models.DateTimeField(auto_now_add=True, null=False)
 
     def __str__(self) -> str:
         return self.bodybuilder.full_name+" "+self.amount
