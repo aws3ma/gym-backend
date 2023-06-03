44d1eef58ce32fa65f4bc0d14aceea7653b87b4c
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-06-03 22:47:40 +0100

    change join_date field

--- bodybuilder/models.py
+++ bodybuilder/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from authentication.models import User
 # Create your models here.
-
+from datetime import date
 
 class BodyBuilder(models.Model):
     full_name = models.CharField(max_length=150,null=False)
     phone = models.CharField(max_length=8,null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    join_date = models.DateTimeField(auto_now_add=True, null=False)
+    join_date = models.DateTimeField(default=date.today, null=False)
     active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
