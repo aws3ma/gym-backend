b365862b51ad0fba590a8e277e3418e803469e02
Oussema Trabelsi <oussema99trabelsi@gmail.com>
2023-06-03 23:00:28 +0100

    fix body builder paiement admin bug

--- bodybuilder/models.py
+++ bodybuilder/models.py
@@ -7,7 +7,7 @@ class BodyBuilder(models.Model):
     full_name = models.CharField(max_length=150,null=False)
     phone = models.CharField(max_length=8,null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    join_date = models.DateTimeField(default=date.today, null=False)
+    join_date = models.DateField(default=date.today, null=False)
     active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
